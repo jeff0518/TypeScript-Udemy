aecc635682a8911381261b70a5df773ef2d933fd
jeff0518 <cycivs07@icloud.com>
2023-03-07 12:49:43 +0800

    feat: 環境設置

---
+++ basics.ts
@@ -0,0 +1,26 @@
+
+
+let course: string | number = 'React - The Complete Guide'
+course = 1234
+
+type Person = {
+  name: string;
+  age: number;
+}
+
+let person: Person = {
+	name: 'jeff',
+  age: 35,
+}
+
+let people: Person[]
+
+function add(a: number, b:number) {
+  return a + b
+}
+
+function print(value: any) {
+  console.log(value);
+  
+}
+
