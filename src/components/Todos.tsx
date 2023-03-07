f3fbdde08b7cf158a5189a02adeb5c7ee9819ae6
jeff0518 <cycivs07@icloud.com>
2023-03-07 15:06:11 +0800

    feat: 學習使用class創立TypeScript 的模型

---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import TodoItem from "./TodoItem"
+import Todo from "../models/todo"
+
+const Todos: React.FC<{ items: Todo[]}> = (props) => {
+  return (
+    <ul>
+      {props.items.map( (item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
+    </ul>
+  )
+}
+
+export default Todos
